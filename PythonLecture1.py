ca23d1f6e5727b410224cfb9c157ff861f3268d2
Sameer Singh <Sameer0Singh123@gmail.com>
2024-08-19 10:21:10 +0530

    Completed variable in python

--- PythonLecture1.py
+++ PythonLecture1.py
@@ -6,5 +6,23 @@
 # print("Hello" + " Sameer Singh")
 
 # Use of the input function
-x = input("What is your name \n")
-print(x)
+# print("Enter your name :")
+# x =input()
+# print("Hello "  + x)
+
+
+# print("Enter your name, age, and your course ")
+# name = input("Name: ")
+# age = input("age: ")
+# course = input("course: ")
+# print("Hello " + name + " you are " + age + " you are enrolled in : " + course)
+
+# calculate the number of character in your name
+# name = input("Enter your name: ")
+# print(len(name))
+
+# Generate a brand name using lastName  of your.
+name = input().strip()
+first = name.split()
+second = " Rock Band"
+yourBandName = print("your band name is " +first[len(first)-1] + second)
