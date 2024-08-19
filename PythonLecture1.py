cd6da50ab800e6859e6f3d7ed900c0241394ee90
Sameer Singh <Sameer0Singh123@gmail.com>
2024-08-19 09:03:54 +0530

    Learning Python

---
+++ PythonLecture1.py
@@ -0,0 +1,10 @@
+# Working with python to manage data
+
+# Use of print function
+# print("Hello world")
+# print("Hello world \nHello world \nHello world")
+# print("Hello" + " Sameer Singh")
+
+# Use of the input function
+x = input("What is your name \n")
+print(x)
